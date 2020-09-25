f03da08aac6712aeb2fadbbe4d233b440155e46a
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-09-25 11:09:47 -0400

    add: yellow color, warning message and trace message

--- io/messages.go
+++ io/messages.go
@@ -7,11 +7,19 @@ import (
 // Reset red ANSI color
 const Reset = "\u001b[0m"
 
-// Red red ANSI color
+// Red ANSI color
 const Red = "\u001b[31m"
 
-// Green red ANSI color
+// Green ANSI color
 const Green = "\u001b[32m"
+
+// Yellow ANSI color
+const Yellow = "\u001b[33m"
+
+var bug = Yellow + "\nlooks like a bug? if you think this, please send us your report:" + Reset + Green + "\n==> https://github.com/eggheaddev/hostify-cli/issues\n" + Reset
+
+// Trace default message
+var Trace = bug + Red + "\n ==== Error trace ==== \n" + Reset
 
 // ErrorMessage print error message
 func ErrorMessage(text string) {
@@ -21,4 +29,9 @@ func ErrorMessage(text string) {
 // SuccessMessage print success message
 func SuccessMessage(text string) {
 	fmt.Println(Green + "Done: " + Reset + text)
+}
+
+// WarningMessage print warning message
+func WarningMessage(text string) {
+	fmt.Println(Yellow + "Warning: " + Reset + text)
 }
